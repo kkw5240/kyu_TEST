da529c6f9db46d8857f2c4fd17cb9dd835f62d2d
kwkim <kkw5240@gmail.com>
2017-05-06 19:48:24 +0900

    2일차
    (cherry picked from commit e9adec8)

---
+++ python/kwkim/2/2_5_2558.py
@@ -0,0 +1,14 @@
+#   A+B - 2
+#   문제
+#       A+B를 계산하시오.
+#   입력
+#       첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
+#   출력
+#       첫째 줄에 A+B를 출력한다. (A+B < 10)
+
+A = int(input())
+B = int(input())
+
+result = A + B
+
+print(result)
